cc852e78703e7e5fabd165df9a1778db0644691f
Himanshu Patel <himanshu.patel_cs19@gla.ac.in>
2021-07-09 23:01:26 +0530

    Final Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ interface Props { }
 
 const App: React.FC<Props> = (props) => {
   const [page, setPage] = useState(1);
-  const [title, setTitle] = useState("Favourite");
   return (
 
     <div>
-      <Userdata pageNumber={page} title={title} />
+      <Userdata pageNumber={page} />
       <button className="text-white py-2 px-4 bg-blue-600" onClick={() => setPage(page + 1)}>
         Next Page
       </button>
