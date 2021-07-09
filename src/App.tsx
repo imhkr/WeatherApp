da06be54cde87034d64baaa30c396170d8ab0eff
Himanshu Patel <himanshu.patel_cs19@gla.ac.in>
2021-07-09 22:04:56 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Userdata from "./Userdata";
+import React, { Dispatch, useState, SetStateAction, Fragment } from "react";
+import axios from "axios";
+interface Props { }
 
-function App() {
+const App: React.FC<Props> = (props) => {
+  const [page, setPage] = useState(1);
+  const [title, setTitle] = useState("Favourite");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Userdata pageNumber={page} title={title} />
+      <button className="px-2 py-2 bg-yellow-900" onClick={() => setPage(page + 1)}>
+        Next
+      </button>
     </div>
   );
 }
